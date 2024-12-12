ba17b5dc025d870897c0971ce97d910ca20e6e1a
YuriiHoliuk <yurii.holiuk@mate.academy>
2024-12-13 00:13:17 +0200

    Initial commit

---
+++ nix/flake.nix
@@ -0,0 +1,177 @@
+{
+  description = "Bionicus's Mac configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, nixpkgs }:
+    let
+      configuration = { pkgs, config, ... }: {
+
+        nixpkgs.config.allowUnfree = true;
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = with pkgs; [
+          neovim
+          mkalias
+          argocd
+          aws-iam-authenticator
+          bat
+          curl
+          fzf
+          git
+          gitAndTools.git-lfs
+          jq
+          kubeseal
+          postgresql_14
+          rsync
+          tmux
+          watch
+          wget
+          zoxide
+          nushell
+          eza
+          yazi
+        ];
+
+        homebrew = {
+          enable = true;
+          brews = [
+            # Add unavailable formulae here
+            "allure"
+            "lsusb"
+            "helm"
+            "kubernetes-cli"
+            "powerlevel10k"
+          ];
+          casks = [
+            "ngrok"
+            "raycast"
+            "wezterm"
+            "font-meslo-lg-nerd-font"
+          ];
+          onActivation.cleanup = "zap";
+        };
+
+        fonts.packages =
+          [
+            (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+          ];
+
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in
+          pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done:
+          '';
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+        services.nix-daemon.enable = true;
+        programs.zsh.enable = true;
+        security.pam.enableSudoTouchIdAuth = true;
+
+        users.users.omerxx.home = "/Users/yuriiholiuk";
+        home-manager.backupFileExtension = "backup";
+        nix.configureBuildUsers = true;
+        nix.useDaemon = true;
+        system.defaults = {
+          dock.autohide = true;
+          dock.mru-spaces = false;
+          finder.AppleShowAllExtensions = true;
+          finder.FXPreferredViewStyle = "clmv";
+          screensaver.askForPasswordDelay = 10;
+        };
+      };
+    in
+      {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#MacBook-Pro-2
+      darwinConfigurations."MacBook-Pro-2" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "yuriiholiuk";
+
+              # Optional: Declarative tap management
+              # taps = {
+              #   "homebrew/homebrew-core" = homebrew-core;
+              #   "homebrew/homebrew-cask" = homebrew-cask;
+              #   "homebrew/homebrew-bundle" = homebrew-bundle;
+              # };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+          home-manager.darwinModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.yuriiholiuk = import ./home.nix;
+          }
+        ];
+      };
+
+      darwinPackages = self.darwinConfigurations."MacBook-Pro-2".pkgs;
+    };
+}
---
+++ nix/home.nix
@@ -0,0 +1,52 @@
+# home.nix
+# home-manager switch 
+
+{ config, pkgs, ... }:
+
+{
+  home.username = "yuriiholiuk";
+  home.homeDirectory = "/Users/yuriiholiuk";
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+# Makes sense for user specific applications that shouldn't be available system-wide
+  home.packages = [
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+    ".zsh_aliases".source = ~/dotfiles/zshrc/.zsh_aliases;
+    ".wezterm.lua".source = ~/dotfiles/wezterm/.wezterm.lua;
+    # ".config/skhd".source = ~/dotfiles/skhd;
+    # ".config/starship".source = ~/dotfiles/starship;
+    # ".config/zellij".source = ~/dotfiles/zellij;
+    ".config/nvim".source = ~/dotfiles/nvim;
+    ".config/nix".source = ~/dotfiles/nix;
+    # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    ".tmux.conf".source = ~/dotfiles/tmux/.tmux.conf;
+    # ".config/ghostty".source = ~/dotfiles/ghostty;
+    # ".config/aerospace".source = ~/dotfiles/aerospace;
+    # ".config/sketchybar".source = ~/dotfiles/sketchybar;
+    # ".config/nushell".source = ~/dotfiles/nushell;
+  };
+
+  home.sessionVariables = {
+  };
+
+  home.sessionPath = [
+    "/run/current-system/sw/bin"
+      "$HOME/.nix-profile/bin"
+  ];
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      # Add any additional configurations here
+      export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+    '';
+  };
+}
